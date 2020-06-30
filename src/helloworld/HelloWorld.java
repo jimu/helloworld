6b869a62afb7e64bc01053ec2167514df9d91175
Jim Urbas <git@jimu.net>
2020-06-30 08:54:36 -0500

    Implemented HelloWorld main class

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ *  this project is a github test
+ */
+package helloworld;
+
+/**
+ * Main executable class
+ * 
+ * @version 0.0.2
+ * @author jimu
+ */
+public class HelloWorld {
+
+	/**
+	 * Prints "Hello World" and first command-line argument if any
+	 * @param args name to display
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World" + (args.length > 0 ? " " + args[0] : ""));
+	}
+
+}
